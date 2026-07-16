975e0df06264e971a21117485aea611a4012747b
Chandrahas10 <142862718+Chandrahas10@users.noreply.github.com>
2026-07-16 14:35:34 +0530

    ✅ Sort 0s, 1s and 2s | Medium | Java | Runtime: N/A | Memory: N/A | 2026-07-16

---
+++ gfg/medium/sort-an-array-of-0s-1s-and-2s4231/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int low =0;
+        int mid =0;
+        int high = arr.length-1;
+        
+        while(mid<=high){
+            switch(arr[mid]){
+                
+                case 0 :{
+                    int temp =arr[low];
+                    arr[low]= arr[mid];
+                    arr[mid]=temp;
+                    low++;mid++;
+                    break;
+                }
+                
+                case 1:{
+                    mid++;
+                    break;
+                }
+                
+                case 2:{
+                    int temp =arr[mid];
+                    arr[mid] = arr[high];
+                    arr[high ]= temp;
+                    high--;
+                    break;
+                }
+                
+            }
+            
+            
+        }
+        
+    }
+}
