f694137a8bbbc2f79a8cb1e68d6aa2f43115facd
Chandrahas10 <142862718+Chandrahas10@users.noreply.github.com>
2026-08-02 20:20:18 +0530

    ✅ Size of Binary Tree | Basic | Java | Runtime: N/A | Memory: N/A | 2026-08-02

--- gfg/basic/size-of-binary-tree/solution.java
+++ gfg/basic/size-of-binary-tree/solution.java
@@ -16,17 +16,9 @@ class Node
 */
 class Solution {
     public int getSize(Node root) {
-        return inOrder(root,0);
-    }
-    
-    public int inOrder(Node root,int cnt){
-        if(root == null)
-            return cnt;
+        if(root == null) return 0;
         
-       cnt= inOrder(root.left,cnt);
-        cnt++;
-       cnt= inOrder(root.right,cnt);
+        return 1+getSize(root.left)+getSize(root.right);
         
-        return cnt;
     }
 }
